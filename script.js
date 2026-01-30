ce6786e96820662ea4b64f304cf923b9c464ba6f
Tart3658 <andrewyohanes3@gmail.com>
2026-01-29 19:46:41 -0500

    Added the create menu
    Adds simple data to the local storage (still a work in progress)

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-window.onload = function () {
-    const request = new XMLHttpRequest();
-    let data;
-    let grid;
 
-  
-    xhr.addEventListener('readystatechange', function () {
-      if (xhr.readyState === 4 && xhr.response === 200) {
-        data = JSON.parse(request.responseText);
-        console.log(data);
-        if(data){
-            let movie = toString(data.Movie);
-        }
-      }
-    });
-  };
-
-  request.open("GET", "file.JSON", true);
-  request.send();
 
